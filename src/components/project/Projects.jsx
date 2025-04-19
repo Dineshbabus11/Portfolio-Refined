c163d877b7bbe84326a115191c3a7dfeba3bf08f
Dineshbabus11 <msgtodineshbabus@gmail.com>
2025-04-19 13:02:32 +0530

    Small update

--- src/components/project/Projects.jsx
+++ src/components/project/Projects.jsx
@@ -8,105 +8,73 @@ import ListItemAvatar from '@mui/material/ListItemAvatar';
 import Avatar from '@mui/material/Avatar';
 import Typography from '@mui/material/Typography';
 
+
 export const Projects = () => {
+    const projectList = [
+        {
+            title: 'ATM Management System',
+            tools: 'Java Oops',
+            link: 'https://your-atm-project-link.com',
+            image: 'atmImage'
+        },
+        {
+            title: 'Pizza Bill Generator',
+            tools: 'Java Oops',
+            link: 'https://your-pizza-project-link.com',
+            image: 'pizzaImage'
+        },
+        {
+            title: 'Food Card App',
+            tools: 'React.js',
+            link: 'https://your-food-card-app.com',
+            image: 'foodAppImage'
+        }
+    ];
+
     return (
         <div className="projects">
             <h1>Projects</h1>
             <div className="projects-content">
                 <div className="content">
                     <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'rgb(78,70,70)' }}>
-                        <ListItem alignItems="flex-start">
-                            <ListItemAvatar>
-                                <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
-                            </ListItemAvatar>
-                            <ListItemText
-                                sx={{ fontFamily: 'poppins' }}
-                                primary={
-                                    <React.Fragment>
-                                        <Typography
-                                            component="span"
-                                            variant="body2"
-                                            sx={{ color: '#ff3838', display: 'inline', fontFamily: 'poppins' }}
-                                        >
-                                            ATM Management System
-                                        </Typography>
-                                    </React.Fragment>
-                                }
-                                secondary={
-                                    <React.Fragment>
-                                        <Typography
-                                            component="span"
-                                            variant="body2"
-                                            sx={{ color: 'white', display: 'inline', fontFamily: 'poppins' }}
-                                        >
-                                            Tools <span>- Java Oops</span>
-                                        </Typography>
-                                    </React.Fragment>
-                                }
-                            />
-                        </ListItem>
-                        <Divider variant="inset" component="li" />
-                        <ListItem alignItems="flex-start">
-                            <ListItemAvatar>
-                                <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
-                            </ListItemAvatar>
-                            <ListItemText
-                                sx={{ fontFamily: 'poppins' }}
-                                primary={
-                                    <React.Fragment>
-                                        <Typography
-                                            component="span"
-                                            variant="body2"
-                                            sx={{ color: '#ff3838', display: 'inline', fontFamily: 'poppins' }}
-                                        >
-                                            Pizza Bill Generator
-                                        </Typography>
-                                    </React.Fragment>
-                                }
-                                secondary={
-                                    <React.Fragment>
-                                        <Typography
-                                            component="span"
-                                            variant="body2"
-                                            sx={{ color: 'white', display: 'inline', fontFamily: 'poppins' }}
-                                        >
-                                            Tools <span>- Java Oops</span>
-                                        </Typography>
-                                    </React.Fragment>
-                                }
-                            />
-                        </ListItem>
-                        <Divider variant="inset" component="li" />
-                        <ListItem alignItems="flex-start">
-                            <ListItemAvatar>
-                                <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
-                            </ListItemAvatar>
-                            <ListItemText
-                                sx={{ fontFamily: 'poppins' }}
-                                primary={
-                                    <React.Fragment>
-                                        <Typography
-                                            component="span"
-                                            variant="body2"
-                                            sx={{ color: '#ff3838', display: 'inline', fontFamily: 'poppins' }}
-                                        >
-                                            Food Card App
-                                        </Typography>
-                                    </React.Fragment>
-                                }
-                                secondary={
-                                    <React.Fragment>
-                                        <Typography
-                                            component="span"
-                                            variant="body2"
-                                            sx={{ color: 'white', display: 'inline', fontFamily: 'poppins' }}
-                                        >
-                                            Tools <span>- React.js</span>
-                                        </Typography>
-                                    </React.Fragment>
-                                }
-                            />
-                        </ListItem>
+                        {projectList.map((project, index) => (
+                            <React.Fragment key={index}>
+                                <ListItem
+                                    alignItems="flex-start"
+                                    button
+                                    component="a"
+                                    href={project.link}
+                                    target="_blank"
+                                    rel="noopener noreferrer"
+                                    sx={{cursor:'pointer'}}
+                                >
+                                    <ListItemAvatar>
+                                        <Avatar alt={project.title} src={project.image} />
+                                    </ListItemAvatar>
+                                    <ListItemText
+                                        primary={
+                                            <Typography
+                                                component="span"
+                                                variant="body2"
+                                                sx={{ color: '#ff3838', fontFamily: 'poppins',fontSize:'1.2rem' }}
+                                            >
+                                                {project.title}
+                                            </Typography>
+                                        }
+                                        secondary={
+                                            <Typography
+                                                component="span"
+                                                variant="body2"
+                                                sx={{ color: 'white', fontFamily: 'poppins' }}
+                                            >
+                                                <br />Tools <span>- {project.tools}</span>
+                                            </Typography>
+                                        }
+                                    />
+                                </ListItem>
+                                {index !== projectList.length - 1 && <Divider variant="inset" component="li" />}
+                            </React.Fragment>
+                        ))}
                     </List>
                 </div>
             </div>
