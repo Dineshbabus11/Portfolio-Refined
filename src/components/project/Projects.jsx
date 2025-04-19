891903dcfd2f5cd15b491704998600b6885b4e5e
Dineshbabus11 <msgtodineshbabus@gmail.com>
2025-04-19 16:22:32 +0530

    Project component completed

--- src/components/project/Projects.jsx
+++ src/components/project/Projects.jsx
@@ -7,6 +7,15 @@ import ListItemText from '@mui/material/ListItemText';
 import ListItemAvatar from '@mui/material/ListItemAvatar';
 import Avatar from '@mui/material/Avatar';
 import Typography from '@mui/material/Typography';
+import LocalAtmIcon from '@mui/icons-material/LocalAtm';
+import LocalPizzaIcon from '@mui/icons-material/LocalPizza';
+import FastfoodIcon from '@mui/icons-material/Fastfood';
+import WbSunnyIcon from '@mui/icons-material/WbSunny';
+import QuizIcon from '@mui/icons-material/Quiz';
+import QrCodeScannerIcon from '@mui/icons-material/QrCodeScanner';
+import CurrencyExchangeIcon from '@mui/icons-material/CurrencyExchange';
+import TuneIcon from '@mui/icons-material/Tune';
+import ParaglidingIcon from '@mui/icons-material/Paragliding';
 
 
 export const Projects = () => {
@@ -14,20 +23,62 @@ export const Projects = () => {
         {
             title: 'ATM Management System',
             tools: 'Java Oops',
-            link: 'https://your-atm-project-link.com',
-            image: 'atmImage'
+            link: 'https://github.com/Dineshbabus11/ATM-Machine-Management-System',
+            image: <LocalAtmIcon sx={{background:'black',color:'#ff3838',width:'45px',height:'45px',borderRadius:'50%'}}/>
         },
         {
             title: 'Pizza Bill Generator',
             tools: 'Java Oops',
-            link: 'https://your-pizza-project-link.com',
-            image: 'pizzaImage'
+            link: 'https://github.com/Dineshbabus11/Pizza_Bill_Generator',
+            image: <LocalPizzaIcon sx={{background:'black',color:'#ff3838',width:'45px',height:'45px',borderRadius:'50%'}}/>
         },
         {
             title: 'Food Card App',
             tools: 'React.js',
-            link: 'https://your-food-card-app.com',
-            image: 'foodAppImage'
+            link: 'https://savoryfoodcart.netlify.app/',
+            image: <FastfoodIcon sx={{background:'black',color:'#ff3838',width:'45px',height:'45px',borderRadius:'50%'}}/>
+        }
+    ];
+
+    const projectList1 = [
+        {
+            title: 'Weather App',
+            tools: 'React.js, OpenWeather API',
+            link: 'https://github.com/Dineshbabus11/weather_app',
+            image: <WbSunnyIcon sx={{background:'black',color:'#ff3838',width:'45px',height:'45px',borderRadius:'50%'}}/>
+        },
+        {
+            title: 'Quiz App',
+            tools: 'React.js',
+            link: 'https://dbsquiz.netlify.app/',
+            image: <QuizIcon sx={{background:'black',color:'#ff3838',width:'45px',height:'45px',borderRadius:'50%'}}/>
+        },
+        {
+            title: 'QR Code Generator',
+            tools: 'React.js, API',
+            link: 'https://merry-crepe-43acce.netlify.app/',
+            image: <QrCodeScannerIcon sx={{background:'black',color:'#ff3838',width:'45px',height:'45px',borderRadius:'50%'}}/>
+        }
+    ];
+
+    const projectList2 = [
+        {
+            title: 'Currency Convertor App',
+            tools: 'React.js, Open API',
+            link: 'https://github.com/Dineshbabus11/Currency-convertor',
+            image: <CurrencyExchangeIcon sx={{background:'black',color:'#ff3838',width:'45px',height:'45px',borderRadius:'50%'}}/>
+        },
+        {
+            title: 'Product Filter',
+            tools: 'HTML, CSS, JS',
+            link: 'https://dbsproduct.netlify.app/',
+            image: <TuneIcon sx={{background:'black',color:'#ff3838',width:'45px',height:'45px',borderRadius:'50%'}}/>
+        },
+        {
+            title: 'BMI Calculator',
+            tools: 'React.js',
+            link: 'https://dbsbmicalulator.netlify.app/',
+            image: <ParaglidingIcon sx={{background:'black',color:'#ff3838',width:'45px',height:'45px',borderRadius:'50%'}}/>
         }
     ];
 
@@ -46,10 +97,108 @@ export const Projects = () => {
                                     href={project.link}
                                     target="_blank"
                                     rel="noopener noreferrer"
-                                    sx={{cursor:'pointer'}}
+                                    sx={{cursor:'pointer',transition:'transform 0.3s ease',
+                                        '&:hover':{
+                                            transform:'scale(1.1)'
+                                        }
+                                    }}
                                 >
                                     <ListItemAvatar>
-                                        <Avatar alt={project.title} src={project.image} />
+                                        {project.image}
+                                    </ListItemAvatar>
+                                    <ListItemText
+                                        primary={
+                                            <Typography
+                                                component="span"
+                                                variant="body2"
+                                                sx={{ color: '#ff3838', fontFamily: 'poppins',fontSize:'1.2rem' }}
+                                            >
+                                                {project.title}
+                                            </Typography>
+                                        }
+                                        secondary={
+                                            <Typography
+                                                component="span"
+                                                variant="body2"
+                                                sx={{ color: 'white', fontFamily: 'poppins' }}
+                                            >
+                                                <br />Tools <span>- {project.tools}</span>
+                                            </Typography>
+                                        }
+                                    />
+                                </ListItem>
+                                {index !== projectList.length - 1 && <Divider variant="inset" component="li" />}
+                            </React.Fragment>
+                        ))}
+                    </List>
+                </div>
+
+                <div className="content">
+                    <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'rgb(78,70,70)' }}>
+                        {projectList1.map((project, index) => (
+                            <React.Fragment key={index}>
+                                <ListItem
+                                    alignItems="flex-start"
+                                    button
+                                    component="a"
+                                    href={project.link}
+                                    target="_blank"
+                                    rel="noopener noreferrer"
+                                    sx={{cursor:'pointer',transition:'transform 0.3s ease',
+                                        '&:hover':{
+                                            transform:'scale(1.1)'
+                                        }
+                                    }}
+                                >
+                                    <ListItemAvatar>
+                                        {project.image}
+                                    </ListItemAvatar>
+                                    <ListItemText
+                                        primary={
+                                            <Typography
+                                                component="span"
+                                                variant="body2"
+                                                sx={{ color: '#ff3838', fontFamily: 'poppins',fontSize:'1.2rem' }}
+                                            >
+                                                {project.title}
+                                            </Typography>
+                                        }
+                                        secondary={
+                                            <Typography
+                                                component="span"
+                                                variant="body2"
+                                                sx={{ color: 'white', fontFamily: 'poppins' }}
+                                            >
+                                                <br />Tools <span>- {project.tools}</span>
+                                            </Typography>
+                                        }
+                                    />
+                                </ListItem>
+                                {index !== projectList.length - 1 && <Divider variant="inset" component="li" />}
+                            </React.Fragment>
+                        ))}
+                    </List>
+                </div>
+
+                <div className="content">
+                    <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'rgb(78,70,70)' }}>
+                        {projectList2.map((project, index) => (
+                            <React.Fragment key={index}>
+                                <ListItem
+                                    alignItems="flex-start"
+                                    button
+                                    component="a"
+                                    href={project.link}
+                                    target="_blank"
+                                    rel="noopener noreferrer"
+                                    sx={{cursor:'pointer',transition:'transform 0.3s ease',
+                                        '&:hover':{
+                                            transform:'scale(1.1)'
+                                        }
+                                    }}
+                                >
+                                    <ListItemAvatar>
+                                        {project.image}
                                     </ListItemAvatar>
                                     <ListItemText
                                         primary={
